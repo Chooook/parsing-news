2fb7a6d8dbb3c8ffebcb7d4d0801e5e3a6e73248
Chook <egr311@ya.ru>
2022-04-12 15:19:14 +0300

    pymorphy2 import moved into functions headers deprecated result initialisation and concat deprecated 'for' cycle implemented into links_parser function old comments deprecated filename is automated now (+launch date as launch_time)

--- script.py
+++ script.py
@@ -1,51 +1,21 @@
-import pymorphy2
+import requests
 
 from functions import *
 
 session = requests.Session()
-# session.headers.update(headers)
 connection_check(session)
 keys = read_keys(files_parse())
 
-result = pd.DataFrame(columns=(
-    'Ключевое слово',
-    'Ссылки',
-    'Заголовок',
-    'Тексты'
-))
-morph = pymorphy2.MorphAnalyzer()
-
-for engine in engines.__iter__():
-
-    links, titles, keys_for_table = links_parser(session, keys, engine, morph)
-
-    result = pd.concat(
-        [result,
-         pd.DataFrame({
-             # 'Поисковик': engine,
-             'Ключевое слово': keys_for_table,
-             'Заголовок': titles,
-             'Ссылки': links
-         })
-         ],
-        ignore_index=True
-    )
+result = links_parser(session, keys)
 
 result.drop_duplicates(['Ссылки'], inplace=True, ignore_index=True)
-# result = drop_by_titles(result)
-# result.drop_duplicates(['Заголовок с поисковика'], inplace=True, ignore_index=True)
-# result.to_excel(directory + '/'
-#                 + 'resultsNoText.xlsx',
-#                 index=False)
 result['Тексты'] = text_parser(result, session)
 # result.to_excel(output_dir + '/'
-#                 + 'results_clear_text_findAll.xlsx',
+#                 + 'results_no_clear_text_.xlsx',
 #                 index=False)
+
 result = clear_texts(result)
-result.to_excel(output_dir + '/'
-                + 'result.xlsx',
+result.to_excel(f'{output_dir}/result_{launch_time}.xlsx',
                 index=False)
 
 session.close()
-
-# response.content.decode('windows-1251') попробовать решить траблы с кодировкой
